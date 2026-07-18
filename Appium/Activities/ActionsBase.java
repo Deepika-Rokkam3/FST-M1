87d358fedeb9f6453546fff58c45e9d98230f83d
Deepika-Rokkam3 <deepikarokkamit@gmail.com>
2026-07-18 14:16:07 +0530

    Add files via upload

---
+++ Appium/Activities/ActionsBase.java
@@ -0,0 +1,14 @@
+package Activities;
+
+import org.openqa.selenium.Point;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class ActionsBase {
+
+	public void doSwipe(AndroidDriver driver, int i, Point start, Point end) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
